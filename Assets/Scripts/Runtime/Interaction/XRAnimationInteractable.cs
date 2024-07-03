d7d7fb6180311b8f6512d4462645543eba20876f
Vladislav <ivanov.vlados94@gmail.com>
2024-07-03 15:58:32 +0300

    feat (interaction) : created logic for playing cupboard animation

---
+++ Assets/Scripts/Runtime/Interaction/XRAnimationInteractable.cs
@@ -0,0 +1,54 @@
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine;
+
+public class XRAnimationInteractable : MonoBehaviour
+{
+    [SerializeField] private XRGrabInteractable _interactable;
+    [SerializeField] private Animator _animator;
+
+    private bool _isDoorClosed;
+
+    private readonly int _doorOpenParameterHash = Animator.StringToHash("DoorOpening");
+    private readonly int _doorCloseParameterHash = Animator.StringToHash("DoorClosing");
+
+    private void OnEnable()
+    {
+        SubscribeToEvents();
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeToEvents();
+    }
+
+    private void SubscribeToEvents()
+    {
+        _interactable.selectEntered.AddListener(OnSelectEntered);
+    }
+
+    private void UnsubscribeToEvents()
+    {
+        _interactable.selectEntered.RemoveListener(OnSelectEntered);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        PlayAnimation();
+    }
+
+    private void PlayAnimation()
+    {
+        if (_isDoorClosed)
+        {
+            _animator.SetBool(_doorOpenParameterHash, true);
+            _animator.SetBool(_doorCloseParameterHash, false);
+            _isDoorClosed = false;
+        }
+        else
+        {
+            _animator.SetBool(_doorOpenParameterHash, false);
+            _animator.SetBool(_doorCloseParameterHash, true);
+            _isDoorClosed = true;
+        }
+    }
+}
