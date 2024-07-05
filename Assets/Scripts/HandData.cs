a7f41a3a3234b9c7f8c2221fd068d53e6edc66b9
Vladislav <ivanov.vlados94@gmail.com>
2024-07-05 10:03:28 +0300

    feat (grab) : added shirt model for grab

---
+++ Assets/Scripts/HandData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandData : MonoBehaviour
+{
+	[SerializeField] private HandModelType _handType;
+	[SerializeField] private Animator _animator;
+	[SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+
+	public HandModelType HandType => _handType;
+
+	public void HandModelSetActive(bool enabled)
+	{
+		_animator.enabled = enabled;
+		_skinnedMeshRenderer.enabled = enabled;
+	}
+}
---
+++ Assets/Scripts/HandModelType.cs
@@ -0,0 +1,5 @@
+public enum HandModelType
+{
+	Left,
+	Right
+}
